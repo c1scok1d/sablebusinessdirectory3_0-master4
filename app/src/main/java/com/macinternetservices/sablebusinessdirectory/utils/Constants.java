65e1c059b12f37029565cf9dea16f718940b99c5
c1scok1d <rchatman@macinternetservices.com>
2021-08-23 23:38:20 -0500

    changed CONST_RADIUS to 10

--- app/src/main/java/com/macinternetservices/sablebusinessdirectory/utils/Constants.java
+++ app/src/main/java/com/macinternetservices/sablebusinessdirectory/utils/Constants.java
@@ -142,7 +142,7 @@ Location Current_Location=new Location("");
 
     //radius key//
     String RADIUS_KEY="RADIUS_KEY";
-    String CONST_RADIUS = "3";
+    String CONST_RADIUS = "10";
 
 
     //region Product
