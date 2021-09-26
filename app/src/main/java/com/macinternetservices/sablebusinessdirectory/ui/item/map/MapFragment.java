dd18bfd4554a8725ef3ebe19eb9d75514e0ae38c
c1scok1d <rchatman@macinternetservices.com>
2021-09-25 19:11:52 -0500

    added custom info window

--- app/src/main/java/com/macinternetservices/sablebusinessdirectory/ui/item/map/MapFragment.java
+++ app/src/main/java/com/macinternetservices/sablebusinessdirectory/ui/item/map/MapFragment.java
@@ -1,19 +1,28 @@
 package com.macinternetservices.sablebusinessdirectory.ui.item.map;
 
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.macinternetservices.sablebusinessdirectory.R;
@@ -23,6 +32,10 @@ import com.macinternetservices.sablebusinessdirectory.ui.common.PSFragment;
 import com.macinternetservices.sablebusinessdirectory.utils.AutoClearedValue;
 import com.macinternetservices.sablebusinessdirectory.utils.Constants;
 
+import java.util.Map;
+
+import static com.macinternetservices.sablebusinessdirectory.utils.GeofenceNotification.getBitmapFromURL;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -30,7 +43,8 @@ public class MapFragment extends PSFragment {
 
     private String latValue = "48.856452647178386";
     private String lngValue = "2.3523519560694695";
-    private String itemName;
+    private String itemName, itemImage, description;
+    private Float rating, totalRatings;
 
     private final androidx.databinding.DataBindingComponent dataBindingComponent = new FragmentDataBindingComponent(this);
     private GoogleMap map;
@@ -51,6 +65,7 @@ public class MapFragment extends PSFragment {
 
         return binding.get().getRoot();
     }
+    //private Marker marker;
 
     private void initializeMap(Bundle savedInstanceState) {
         try {
@@ -66,12 +81,40 @@ public class MapFragment extends PSFragment {
 
         binding.get().mapView.getMapAsync(googleMap -> {
             map = googleMap;
+            CustomInfoWindowAdapter adapter = new CustomInfoWindowAdapter(getActivity(), rating, itemImage, totalRatings);
 
-//            map.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(new LatLng(Double.valueOf(latValue), Double.valueOf(lngValue))).zoom(1).bearing(10).tilt(10).build()));
+            Glide.with(getActivity())
+                    .asBitmap()
+                    .load(itemImage)
+                    .fitCenter()
+                    .into(new SimpleTarget<Bitmap>(500,500) {
+                        @Override
+                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                            //marker
+                            MarkerOptions markerOpt = new MarkerOptions();
+                            markerOpt.position(new LatLng(Double.parseDouble(latValue), Double.parseDouble(lngValue)))
+                                    .title(itemName)
+                                    .snippet(description);
+                                    //.icon(BitmapDescriptorFactory.fromBitmap(resource));
+                            //Set Custom InfoWindow Adapter
+                            map.setInfoWindowAdapter(adapter);
+                            map.addMarker(markerOpt).showInfoWindow();
+                        }
 
-            map.addMarker(new MarkerOptions()
-                    .position(new LatLng(Double.parseDouble(latValue), Double.parseDouble(lngValue)))
-                    .title(itemName));
+                        @Override
+                        public void onLoadFailed(@Nullable Drawable errorDrawable) {
+                            super.onLoadFailed(errorDrawable);
+                            //marker
+                            MarkerOptions markerOpt = new MarkerOptions();
+                            markerOpt.position(new LatLng(Double.parseDouble(latValue), Double.parseDouble(lngValue)))
+                                    .title(itemName)
+                                    .snippet(description);
+                                    //.icon(BitmapDescriptorFactory.fromResource(R.mipmap.logo_blk));
+                            //Set Custom InfoWindow Adapter
+                            map.setInfoWindowAdapter(adapter);
+                            map.addMarker(markerOpt).showInfoWindow();
+                        }
+                    });
 
             //zoom
             if (!latValue.isEmpty() && !lngValue.isEmpty()) {
@@ -79,10 +122,8 @@ public class MapFragment extends PSFragment {
                 // Animating to the touched position
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(Double.parseDouble(latValue), Double.parseDouble(lngValue)), zoomlevel));
             }
-
         });
     }
-
 
     @Override
     public void onDestroyView() {
@@ -151,6 +192,10 @@ public class MapFragment extends PSFragment {
             latValue = getActivity().getIntent().getStringExtra(Constants.LAT);
             lngValue = getActivity().getIntent().getStringExtra(Constants.LNG);
             itemName = getActivity().getIntent().getStringExtra(Constants.ITEM_NAME);
+            itemImage = "https://api.sablebusinessdirectory.com/uploads/" +getActivity().getIntent().getStringExtra(Constants.ITEM_IMAGE_URL);
+            description = getActivity().getIntent().getStringExtra(Constants.ITEM_DESCRIPTION);
+            rating = getActivity().getIntent().getFloatExtra(Constants.RATING_STARS,0);
+            totalRatings = getActivity().getIntent().getFloatExtra(Constants.TOTAL_RATINGS, 0);
         }
 
     }
