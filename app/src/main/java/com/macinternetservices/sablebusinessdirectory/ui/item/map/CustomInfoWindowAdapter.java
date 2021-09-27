963e0dc571ecb5a504895fac640e30f4dc8e5f9a
c1scok1d <rchatman@macinternetservices.com>
2021-09-27 11:32:29 -0500

    changed ratingbar and rating text behavior

--- app/src/main/java/com/macinternetservices/sablebusinessdirectory/ui/item/map/CustomInfoWindowAdapter.java
+++ app/src/main/java/com/macinternetservices/sablebusinessdirectory/ui/item/map/CustomInfoWindowAdapter.java
@@ -23,19 +23,21 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
     private Activity context;
     private Float rating, totalRatings;
-    private String image;
+    private String image, description;
 
-    public CustomInfoWindowAdapter(Activity context, Float rating, String image, Float totalRatings){
+    public CustomInfoWindowAdapter(Activity context, Float rating, String image, Float totalRatings, String description){
         this.context = context;
         this.rating = rating;
         this.image = image;
         this.totalRatings = totalRatings;
+        this.description = description;
     }
 
     @Override
     public View getInfoWindow(Marker marker) {
         return null;
     }
+
 
     @Override
     public View getInfoContents(Marker marker) {
@@ -45,14 +47,22 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
         TextView ratingNo = view.findViewById(R.id.ratingNo);
         ImageView itemImage = view.findViewById(R.id.imageView22);
         RatingBar ratingBar = view.findViewById(R.id.ratingBar2);
+        TextView tvDescription = view.findViewById(R.id.custom_window_description);
         ratingBar.setStepSize(0.5f);
         tvTitle.setText(marker.getTitle());
 
-        if (rating == 0.0) {
-            ratingNo.setText(R.string.item_detail__rating);
+        if(!description.isEmpty()){
+            tvDescription.setText(description);
         } else {
-            ratingNo.setText(rating+ " " +"(" +totalRatings+ " ratings)");
-            //ratingNo.setText(R.string.rating__total_count_n_value, rating + "", totalRatings + "");
+            tvDescription.setVisibility(View.GONE);
+        }
+        if (rating == 0.0) {
+            ratingBar.setVisibility(View.GONE);
+            ratingNo.setVisibility(View.GONE);
+        } else {
+            ratingBar.setVisibility(View.VISIBLE);
+            ratingNo.setText(rating+ " " +"(" +Integer.parseInt(String.valueOf(totalRatings))+ " ratings)");
+            //ratingNo.setText(R.string.rating__total_count_n_value, Integer.valueOf(rating + "", Integer.parseInt(totalRatings + "")));
         }
 
         ratingBar.setRating(rating);
